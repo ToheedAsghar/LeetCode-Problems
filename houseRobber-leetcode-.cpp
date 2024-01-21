a8d5ec4fe9cbb62383501036feaec582f84f4fa0
Toheed Asghar <121859513+ToheedAsghar@users.noreply.github.com>
2024-01-21 09:30:37 +0500

    Update houseRobber-leetcode-.cpp

--- houseRobber-leetcode-.cpp
+++ houseRobber-leetcode-.cpp
@@ -9,30 +9,30 @@ int main(){
     cout << rob(arr);
 }
 
-int robDP(vector<int>& nums, vector<int>& memo, int index){
-    int n = memo.size();
-    if(index >= n){             // base case
-        return INT_MIN;
-    }
-
-    if(index == n-1 ||  index == n-2){
-        return memo[index] = nums[index];
-    }
-
-    if(-1 == memo[index]){
-        int maxValue = 0;
-        for(int i=index+2; i<n; i++){
-            memo[index] = max(memo[index], nums[index] + robDP(nums, memo, i));
-        }
-    }
-    return memo[index];
-}
-
 int rob(vector<int>& nums) {
     int n = nums.size();
     vector<int> memo(n, -1);
-    int retVal1 = robDP(nums, memo, 0);
-    int retVal2 = robDP(nums, memo, 1);
+
+    function<int(int)> robDP = [&](int index) -> int { // using lamda function
+        if (index >= n) {
+            return INT_MIN;
+        }
+
+        if (index == n - 1 || index == n - 2) {
+            return memo[index] = nums[index];
+        }
+
+        if (memo[index] == -1) {
+            int maxValue = 0;
+            for (int i = index + 2; i < n; i++) {
+                memo[index] = max(memo[index], nums[index] + robDP(i));
+            }
+        }
+        return memo[index];
+    };
+
+    int retVal1 = robDP(0);
+    int retVal2 = robDP(1);
 
     return max(retVal1, retVal2);
 }
