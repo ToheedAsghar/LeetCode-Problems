ba8b757790728bcad497e5a7427495a21841382c
Toheed Asghar <121859513+ToheedAsghar@users.noreply.github.com>
2024-06-03 17:14:47 +0500

    June/03/2024

---
+++ 2486-AppendCharacterstoStringtoMakeSubsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        i: int = 0
+        j: int = 0
+        n: int = len(s)
+        m: int = len(t)
+
+        while i < n and j < m:
+            if s[i] == t[j]:
+                j += 1
+            i += 1
+
+        if j == m:
+            return 0
+        else:
+            return m - j
